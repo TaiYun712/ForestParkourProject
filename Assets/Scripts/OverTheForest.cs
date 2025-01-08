c2f9f2f909c2c04c6b97ad26c86316ab129d917f
TaiYun712 <wendy0712000@gmail.com>
2025-01-08 18:15:11 +0800

    加上故事提示板、狼 提示板、安全提示板，在狼森林終點加上限制

---
+++ Assets/Scripts/OverTheForest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverTheForest : MonoBehaviour
+{
+    public GameObject safeHint;
+
+    public Collider coll;
+   
+    void Start()
+    {
+        safeHint.SetActive(false);
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            safeHint.SetActive(true);
+            Invoke("CloseTheHint",3f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            coll.isTrigger = false;
+
+        }
+    }
+
+    void CloseTheHint()
+    {
+        safeHint.SetActive(false);
+    }
+
+}
